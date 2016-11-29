f7e8ad76d80bdfa7ff023abe472a80e6be819de7
Dennis Klarenbeek <dennisklarenbeek@MacBook-Pro-van-Dennis.local>
2016-11-29 11:49:34 +0100

    add mock information

--- js/controllers/specificOhcController.js
+++ js/controllers/specificOhcController.js
@@ -4,10 +4,61 @@ var app = angular.module('salesPortal');
 app.controller('specificOhcController', function($scope, $http, $routeParams, $location, $rootScope, dataServices, sortDocumentsServices, alertMessServices) {
 
   var id = $location.path();
-  $scope.typeSmartModules = ['Fuel', 'Shop', 'Car', 'Service', 'Park'];
-  $scope.typeSmartProducts = [{'description': 'AutoAlert', 'time': 0.15},{'description': 'PumpCheck', 'time': 1}]
-  $scope.typeSmartServices = ['Portal', 'Watch-it', 'Snapshot', 'Heartbeat 2.0', 'BBI'];
-  $scope.typeRemote = ['Open VPN', 'Tokheim', '4G']
+  $scope.typeSmartModules = [];
+  $scope.typeSmartProducts = [];
+  $scope.typeSmartServices = [];
+  $scope.typeRemote = [];
+  $scope.test = [];
+  $scope.totalAmount = 0;
+
+  //function for push the products in the right array
+  function categoryArray(data, value, array) {
+    for (var i = 0, iLen=data.length; i<iLen; i++) {
+      if(data[i].category === value) {
+        array.push(data[i]);
+      }
+    }
+  }
+
+  function getOtherInfo(data, value, array) {
+    for (var i = 0, iLen=data.length; i<iLen; i++) {
+      var y = $scope.products.findIndex(x => x.description==data[i].description)
+      data[i].ohc_time = $scope.products[y].ohc_time;
+      data[i].price = $scope.products[y].price;
+      data[i].billing = $scope.products[y].billing;
+      console.log(data[i]);
+    }
+  }
+
+  function calcServices(services) {
+    for (var i = 0, iLen=services.length; i<iLen; i++) {
+      $scope.totalAmount += services[i].ohc_time;
+    }
+  }
+
+  function calcCameras(type, datakey) {
+    var y = $scope.products.findIndex(x => x.description==type);
+    var qCam = datakey;
+    var ohcTime = $scope.products[y].ohc_time;
+    var total = qCam * ohcTime;
+    $scope.totalAmount += total;
+  }
+
+  $scope.testbutton = function calculateOhc() {
+    $scope.totalAmount = 0;
+    calcServices($scope.document.services);
+    console.log($scope.totalAmount);
+    calcCameras("Binnen camera's", $scope.document.insidecameras);
+    console.log($scope.totalAmount);
+  }
+
+  $http.get('mock/product.json').success(function(data){
+    $scope.products = data;
+    categoryArray(data, 'Slimme modules', $scope.typeSmartModules);
+    categoryArray(data, 'Slimme producten', $scope.typeSmartProducts);
+    categoryArray(data, 'Slimme diensten', $scope.typeSmartServices);
+    categoryArray(data, 'Remote verbinding', $scope.typeRemote);
+  });
 
   // GET specific document
   dataServices.get(id).then(function successCallback(response){
@@ -19,16 +70,17 @@ app.controller('specificOhcController', function($scope, $http, $routeParams, $l
 
   //PUT specific document
   $scope.saveDocument = function(customerData) {
-    console.log(customerData);
     dataServices.put(id, customerData).then(function successCallback(response) {
         var message = response.config.data.company + " is bijgewerkt";
         alertMessServices.success(message);
         $rootScope.editing = false;
+        getOtherInfo($scope.document.services, 'Shop', $scope.test);
     //Error Callback
     }, function errorCallback(response){
       var errorMessage = response.statusText + ' ' + response.data.company;
       alertMessServices.error(errorMessage);
     })
+
   }
 
   $scope.deleteDocument = function() {
